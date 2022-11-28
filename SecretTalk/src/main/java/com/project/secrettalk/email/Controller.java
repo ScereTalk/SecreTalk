d2a54241007419fd86ff29dbf7bd91c5d95d5a55
lunashp <hijnshp@gmail.com>
2022-11-28 22:41:23 +0900

    add email check

---
+++ SecretTalk/src/main/java/com/project/secrettalk/email/Controller.java
@@ -0,0 +1,34 @@
+package com.project.secrettalk.email;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class Controller {
+    @Autowired
+    EmailService emailService;
+
+
+
+    @PostMapping("/emailConfirm")
+    @ApiOperation(value = "회원 가입시 이메인 인증", notes = "기존사용하고 있는 이메일을 통해 인증")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BaseResponseBody> emailConfirm(
+            @RequestBody @ApiParam(value="이메일정보 정보", required = true) String email) throws Exception {
+
+     //   EmailService emailService = null;
+        String confirm = emailService.sendSimpleMessage(email);
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, confirm));
+    }
+}
---
+++ SecretTalk/src/main/java/com/project/secrettalk/email/EmailService.java
@@ -0,0 +1,5 @@
+package com.project.secrettalk.email;
+
+public interface EmailService {
+    String sendSimpleMessage(String to)throws Exception;
+}
